199ead954dd779ac0762188bbfd64cc207e2b670
Will-Peterson <william.from.ca@gmail.com>
2021-01-12 16:41:13 -0800

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import Converter from './components/Converter';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='title'>
+        Number to Roman Numeral Converter
+      </div>
+      <div className='form-container'>
+        <Form />
+        <Converter />
+      </div>
     </div>
   );
 }
